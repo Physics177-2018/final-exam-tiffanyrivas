f89f418a7591543c45c828e11e841ff15ed1e0ff
tiffanyrivas <38119683+tiffanyrivas@users.noreply.github.com>
2018-06-15 21:26:08 -0700

    :)

---
+++ P177 Final Exam.py
@@ -0,0 +1,55 @@
+
+# coding: utf-8
+
+# # P177: Final Exam
+# Spring 2018
+# 
+# **Due:** please complete by **Friday, June 15th**.  
+# Pro-tip: do this way before then. 
+# 
+# **iEval:** please complete the [iEval](https://ieval.ucr.edu/) before **Friday, June 8th**. 
+# 
+# 
+# *Tiffany Rivas*
+
+# ## 1. Kaggle Tutorial 
+# 
+# 1. Create a Kaggle account and navigate to the "[Titanic: Machine Learning from Disaster](https://www.kaggle.com/c/titanic)" competition. Read a bit about the challenge.
+# 
+# 2. Navigate to Helge Bjorland's "[Interactive Data Science Tutorial](https://www.kaggle.com/helgejo/an-interactive-data-science-tutorial)" that uses the Titanic dataset. (**Note:** in class we used [Rochelle Silva's tutorial](https://www.kaggle.com/rochellesilva/simple-tutorial-for-beginners), the Bjorland tutorial is probably a little more insightful.) 
+# 
+# 3. **Fork** the tutorial and go through it. Run everything. You are not required to complete the "exercises" (cells where you have to type code), but you get extra credit if you do.
+# 
+# 4. Download the completed tutorial as a Jupyter notebook (`ipynb` file). Upload that notebook to your GitHub repository for this final.
+
+# ## 2. Answer the Following Questions
+# 
+# These are real questions where I'd like you to write a few sentences about your code. The goal is to show that you've thought carefully about your code and can say something insightful about your work. 
+
+# ### a) What was your favorite homework problem? What did you learn from doing it?
+# 
+# *I guess i liked 4a because i was able to create my own graph and actually able to label it*
+
+# ### b) What was the most difficult problem? What was confusing or frustrating about it?
+# 
+# *8b is still confusing I guess my mind is elsewhere when I am doing the code free hand*
+
+# ### c) In your own words, give an example of a problem where you would use Markov Chain Monte Carlo (MCMC). Briefly describe how MCMC works.
+# 
+# *MCMC is just a way to find the average of multiple graphs for example take the final grades of previous years to find the average of the overall class preformance in a desired time span*
+
+# ### d) ELI5: Pick one technical idea from the course and explain it to a five-year-old.
+# 
+# *Riemann Sum is just a way to fill up a giant circle with a bunch of small squares. then you can add up the amount of space left that doesnt completely fill with the squares then from there you can get almost the correct amount that fills up the giant circle*
+
+# ## 3. Evaluation (not for a grade)
+# 
+# I hope you filled out the `iEval` before Jun 8th. In this cell, please feel free to share constructive criticism about the course. What did you like? What did you not like? Are there topics that you really wish we covered or didn't cover? What should I know to be a better lecturer?
+# 
+# *I really liked the course! I just wished it wasn't offered only in the spring quarter becasue I was really burned out from other demanding physics classes*
+
+# ## Closing thoughts
+# 
+# It's been a real joy to be part of this class---I was really impressed by each of your midterm presentations and have had a lot of fun with our discussions. In whatever is ahead of you beyond this class, I look forward to hearing how you each succeed.
+# 
+# I encourage you to make your GitHub repositories public (you may need to duplicate them) so that you can show them off to the world.
